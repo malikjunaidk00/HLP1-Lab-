b1a668b0b13a8e3d1e9ae575a0a3696f0f79cb00
MUHAMMAD JUNAID KHALIL <79202697+malikjunaidk00@users.noreply.github.com>
2022-04-28 16:29:58 +0200

    Add files via upload

---
+++ chp7/drills/drill10.cpp
@@ -0,0 +1,421 @@
+/*
+	The first working version of Stroustrup's calculator.
+	Written by Renátó Besenczi based on the book BS, "Programming: Principles and Practice using C++, Second Edition"
+	See chapter 6 and chapter 7 of book: https://stroustrup.com/programming.html
+	© 2020 Renátó Besenczi, according to CC0. Questions: renato.besenczi@gmail.com
+	Input from cin; output to cout. Input comes from cin through the Token_stream called ts.
+	Grammar:
+	
+	Calculation:
+		Statement
+		Print
+		Quit
+		Calculation Statement
+	Print:
+		;
+	Quit:
+		q
+	Statement:
+		Declaration
+		Expression
+	Declaration:
+		"let" Name "=" Expression
+	Name:
+		string literal
+	Expression:
+		Term
+		Expression + Term
+		Expression – Term
+	Term:
+		Primary
+		Term * Primary
+		Term / Primary
+		Term % Primary
+	Primary:
+		Number
+		( Expression )
+		– Primary
+		+ Primary
+	Number:
+		floating-point-literal
+*/
+
+#include "std_lib_facilities.h" //Include usual header
+
+// Some symbolic constants for clarity
+
+constexpr char number = '8';
+constexpr char quit = 'q';
+constexpr char print = ';';
+constexpr char name = 'a';
+const char let = '#';										//declaring let is equal to '#'	a char not a keyword		--drill10
+constexpr char result = '=';
+
+constexpr char sqrttoken = 'S';                             // declaration of char for sqrt token                     --drill07
+const string sqrtkey = "sqrt";                              // declaration like 'let' for storing string 'sqrt'       --drill07
+
+constexpr char powtoken = 'P';                             // declaration of char for power token                    --drill08
+const string powkey = "pow";                                // declaration like 'let' for storing string 'pow'      --drill08
+
+
+//function declarations
+double expression();
+double primary();
+double term();
+double declaration();
+double statement();
+
+double sqrt_calculation();                                  //  function declaration  --drill07
+
+double pow_calculation();                                   // function declaration  --drill08
+
+// Variable ///////////////////////
+class Variable {
+public:
+	string name;
+	double value;
+};
+
+vector<Variable> var_table;
+
+bool is_declared(string var)
+{
+	for (const auto& v : var_table)
+		if (v.name == var) return true;
+	return false;
+}
+
+double define_name (string var, double val)
+{
+	if (is_declared(var)) error(var, " declared twice");
+	var_table.push_back(Variable{var,val});
+	return val;
+}
+
+double get_value(string s)
+{
+	for(const auto& v : var_table)
+		if (v.name == s) return v.value;
+	error("get: undefined variable", s);
+	return -1;
+}
+
+void set_value(string s, double d)
+{
+	for (auto& v : var_table)
+		if(v.name == s){
+			v.value = d;
+			return;
+		}
+	error("set: undefined variable", s);
+}
+// Variable end ///////////////////////
+
+// Token ///////////////////////
+class Token {
+public:
+	char kind;
+	double value;
+	string name;
+	Token(): kind(0) {}
+	Token(char ch): kind(ch), value(0) {}
+	Token(char ch, double val): kind(ch), value(val) {}
+	Token(char ch, string n): kind(ch), name(n) {}
+};
+// Token end ///////////////////////
+
+// Token_stream ///////////////////////
+class Token_stream {
+public:
+	Token_stream();
+	void putback(Token t);
+	Token get();
+	void ignore(char c);
+private:
+	bool full;
+	Token buffer;
+};
+
+Token_stream::Token_stream() :full(false), buffer(0) {}
+
+void Token_stream::putback(Token t)
+{
+	if (full) error("putback() into full buffer");
+	buffer = t;
+	full = true;
+}
+
+Token Token_stream::get()
+{
+	if (full)
+	{
+		full = false;
+		return buffer;
+	}
+
+	char ch;
+	cin >> ch;
+
+	switch (ch)
+	{
+		case print:
+		case quit:
+		case '(':
+		case ')':
+		case '+':
+		case '-':
+		case '*':
+		case '/':
+		case '%':
+		case '=':
+        case ',':                                           // comma separator for power formulation between base and exponent  --drill08
+		case let:											//as declaration is now a char not a keyword so defining case here Token of type char 	--drill10
+			return Token(ch);
+		case '.':
+		case '0': case '1': case '2': case '3': case '4':
+    	case '5': case '6': case '7': case '8': case '9':
+    	{
+    		cin.putback (ch);
+    		double val;
+    		cin >> val;
+    		return Token(number, val);
+    	}
+    	default: 
+    	{
+    		if (isalpha(ch))
+    		{
+    			string s;
+    			s += ch;
+    			while (cin.get(ch) && (isalpha(ch) || isdigit(ch))) s += ch;
+    			cin.putback(ch);
+    		//	if (s == declkey) return Token{let};											//no need as  we are already checking in case character  & '#' is not a keyword to be checked for strings
+
+            	if (s == sqrtkey) return Token{sqrttoken};                                     //sqrtkey checking --drill07
+                
+                else if (s == powkey) return Token{powtoken};                                     //powkey checking --drill08
+    			
+                else if (is_declared(s))
+    				return Token(number, get_value(s));
+    			return Token{name,s};
+    		}
+    		error("Bad token");
+    	}
+    	return 0;
+	}
+}
+
+void Token_stream::ignore(char c)
+{
+	if (full && c == buffer.kind)
+	{
+		full = false;
+		return;
+	}
+
+	full = false;
+
+	char ch = 0;
+	while (cin>>ch)
+		if (ch==c) return;
+}
+// Token_stream end ///////////////////////
+Token_stream ts;
+
+void clean_up_mess()
+{
+	ts.ignore(print);
+}
+
+// Logically separate calculate() from the usual "boiler plate" of main 
+void calculate()
+{
+	while (cin)
+	try {
+		Token t = ts.get();
+		while (t.kind == print) t = ts.get();
+		if (t.kind == quit) return;
+		ts.putback(t);
+		cout << result << statement() << endl;
+	}
+	catch (exception& e) {
+		cerr << e.what() << endl;
+		clean_up_mess();
+	}
+}
+
+int main() 
+try {
+	define_name("k", 1000);									// drill 06 predefining constant k = 1000
+	define_name("pi", 3.1415926535);
+	define_name("e", 2.7182818284);
+	
+	calculate();
+
+	return 0;
+
+} catch (exception& e){
+	cerr << e.what() << endl;
+	return 1;
+} catch (...) {
+	cerr << "exception\n";
+	return 2;
+}
+
+double sqrt_calculation()                                //defining the squareroot calculation function     --drill07
+{
+    char ch;                                            //since we need to check open paranthesis           --drill07
+    if (cin.get(ch) && ch != '(') 
+        error("'(' is required at starting of sqrt");
+    cin.putback(ch);                                    //need to putback the last entry so that we may check sqrt is ending with ')'       --drill07
+
+    double d = expression();
+
+    if(d < 0) error("value under squareroot is negative");      //exception handling in case of negative value under sqrt       --drill09
+
+    return sqrt(d);
+
+}
+
+double pow_calculation()                                                //power calculation function definitaion   --drill08
+{
+    Token t = ts.get();                                                 //getting input for pow --drill08
+    if (t.kind != '(') error("'(' is expected in starting of pow");     //exception handling in case of no opening paranthesis               --drill08
+
+    double base = expression();                                         //getting base expression --drill08        
+
+    t = ts.get();
+    if (t.kind != ',') error("',' expected in after base in pow");      //exception handling for missing comma separater of pow formula       --drill08
+
+    double temp_exponent = expression();
+    int exponent = narrow_cast<int>(temp_exponent);                     //exception handling incase of floating value of exponent             --drill08
+
+    t = ts.get();
+    if (t.kind != ')') error("')' is expected at the ending of power formula"); // exception handling in case of missing pow closing paranthesis --drill08
+
+    return pow(base, exponent);
+}
+
+//Functions mapping grammar rules //////////////////////////
+double primary()
+{
+	Token t = ts.get();
+	switch (t.kind)
+	{
+		case '(':
+		{
+			double d = expression();
+			t = ts.get();
+			if (t.kind != ')') error("')' expected");
+			return d;
+		}
+		case number:
+			return t.value;
+		case '-':
+			return - primary();
+		case '+':
+			return primary();
+        case sqrttoken:
+            return sqrt_calculation();                              //inserting in primary so that grammer rules are followed as it is primary function     --drill07
+        case powtoken:
+            return pow_calculation();                              //inserting in primary so that grammer rules are followed as it is primary function      --drill08
+		default:
+			error("primary expected");
+	}
+	return -1;
+}
+
+double term()
+{
+	double left = primary();
+	Token t = ts.get();
+	while(true)
+	{
+		switch (t.kind)
+		{
+			case '*':
+				left *= primary();
+				t = ts.get();
+				break;
+			case '/':
+			{
+				double d = primary();
+				if (d == 0) error("divide by zero");
+				left /= d;
+				t = ts.get();
+				break;
+			}
+			case '%':
+			{
+				double d = primary();
+				if (d == 0) error("%: divide by zero");
+				left = fmod (left, d);
+				t = ts.get();
+				break;
+				/* //or you can use narrow cast check
+				int i1 = narrow_cast<int>(left);
+				int i2 = narrow_cast<int>(primary());
+				if (i2 == 0) error ("%: Zero oszto");
+				left = i1 % i2;
+				t = ts.get();
+				break;
+				*/
+			}
+			default:
+				ts.putback(t);
+				return left;
+		}
+	}
+}
+
+double expression()
+{
+	double left = term();
+	Token t = ts.get();
+	while (true)
+	{
+		switch(t.kind)
+		{
+			case '+':
+				left += term();
+				t = ts.get();
+				break;
+			case '-':
+				left -= term();
+				t = ts.get();
+				break;
+			default:
+				ts.putback(t);
+				return left;
+		}
+	}
+}
+
+double declaration()
+{
+	Token t = ts.get();
+	if (t.kind != name) error("name expected in declaration");
+	string var_name = t.name;
+
+	Token t2 = ts.get();
+	if (t2.kind != '=') error("= missing in declaration of ", var_name);
+
+	double d = expression();
+	define_name(var_name, d);
+	return d;
+}
+
+double statement()
+{
+	Token t = ts.get();
+	switch(t.kind)
+	{
+		case let:
+			return declaration();
+		default:
+			ts.putback(t);
+			return expression();
+	}
+}
+//Functions mapping grammar rules end //////////////////////////
+
+// Program end!
